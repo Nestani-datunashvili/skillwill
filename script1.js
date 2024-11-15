65c73a06646d69d67f793fcf957e072fb8e6eb95
Nestani Datunashvili <nestani.datunashvili@gmail.com>
2024-11-15 22:56:36 +0400

    sixth

--- script1.js
+++ script1.js
@@ -1,66 +1,36 @@
-// task1============
-// const randomText = document.getElementById("random__text");
-// const hideBtn = document.getElementById("hide");
-
-// hideBtn.addEventListener("click", () => {
-//   randomText.classList.toggle("hidden");
-
-//   randomText.classList.contains("hidden")
-//     ? (hideBtn.textContent = "show me")
-//     : (hideBtn.textContent = "hide me");
-// });
-
-// task2============
-
-// const container = document.createElement("div");
-// container.id = "card";
-
-// const title = document.createElement("h2");
-// title.textContent = "Gandalf";
-
-// const link = document.createElement("a");
-// link.setAttribute("href", "#");
-// link.textContent = "Go to profile";
-
-// container.appendChild(title);
-// container.appendChild(link);
-// document.body.appendChild(container);
-
-// task 3 =============
-let score = 0;
-
-function showQuestion(question, answers, correctAnswerIndex) {
-  const questionContainer = document.getElementById("question");
-  questionContainer.textContent = question;
-
-  const answerContainer = document.querySelectorAll(".answer");
-  answerContainer.forEach((element, index) => {
-    element.textContent = answers[index];
-    element.classList.remove("correct", "incorrect");
-    element.onclick = () => checkAnswer(element);
-  });
-}
-
-let checkAnswer = (selected) => {
-  const answers = document.querySelectorAll(".answer");
-  const selectedAnswerIndex = Array.from(answers).indexOf(selected);
-
-  if (selectedAnswerIndex === correctAnswerIndex) {
-    selected.classList.add("correct");
-    score++;
-  } else {
-    selected.classList.add("incorrect");
-  }
-  updateScore();
+const handleModal = () => {
+  const modal = document.getElementById("modalContainer");
+  modal.classList.toggle("active");
 };
 
-function updateScore() {
-  const scoreContainer = document.getElementById("score");
-  scoreContainer.textContent = `${score}`;
-}
+const handleBgColor = () => {
+  const colors = ["red", "blue", "green", "black", "white"];
+  const input = document.getElementById("input");
+  const inputValue = document.getElementById("input").value.toLowerCase();
 
-const question = "what is the capital of Tbilisi";
-const answers = ["tbilisi", "zugdidi", "chxorotsku"];
-const correctAnswerIndex = 0;
+  if (colors.includes(inputValue)) {
+    document.body.style.backgroundColor = `${inputValue}`;
+    input.value = "";
+  } else {
+    alert("enter color from: blue, green, black, white");
+  }
+};
 
-showQuestion(question, answers, correctAnswerIndex);
+const handleNumberAvarge = () => {
+  const result = document.getElementById("avarageResult");
+  const input = document.getElementById("number_input");
+  const inputValue = document
+    .getElementById("number_input")
+    .value.split(":")
+    .map(Number);
+
+  if (!input.value.trim() || inputValue.some(isNaN)) {
+    alert("Enter valid numbers separated by ':'");
+    return;
+  }
+
+  const average =
+    inputValue.reduce((acc, number) => acc + number, 0) / inputValue.length;
+
+  result.textContent = `avarage of those numbers is ${average}`;
+};
